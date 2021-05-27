a1b3390fbd55f0ba306c6c6e4aab11c42635a14c
João Guio <joao.guio@g.globo>
2021-05-27 12:06:42 -0300

    should always decrease sell_in

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -23,8 +23,8 @@ class GildedRose
                  else
                    -1
                  end
+        item.sell_in -= 1
         if (item.quality >= 0) && (item.quality <= 50)
-          item.sell_in -= 1
           item.quality += factor
           item.quality += factor if item.sell_in.negative?
         end
